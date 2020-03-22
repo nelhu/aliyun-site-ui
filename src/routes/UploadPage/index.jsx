60f486b634413aa03a0eb90562ecc18ef66d29ce
huxuezi <xuezierhu@gmail.com>
2020-03-23 00:25:47 +0800

    fix: add desc

--- src/routes/UploadPage/index.jsx
+++ src/routes/UploadPage/index.jsx
@@ -3,8 +3,9 @@ import React from 'react';
 import {connect} from 'react-redux';
 // import API from '@/services';
 import superagent from 'superagent';
-import {Input, Icon, message, List, Progress} from 'antd';
+import {Input, Icon, message, List, Progress, PageHeader} from 'antd';
 import {copy} from '@utils';
+import Footer from '@components/Footer';
 import('./style.postcss');
 
 class UploadPage extends React.Component {
@@ -56,40 +57,44 @@ class UploadPage extends React.Component {
   };
   render() {
     return (
-      <div className="page page-upload">
-        <br />
-        <Input
-          addonAfter={
-            <span onClick={this.trigger}>
-              <Icon type="folder-open" />
-            </span>
-          }
-          placeholder="暂未选择任何文件"
-          value={this.state.fileNames}
-          disabled
-        />
-        <input id="file_chooser" multiple type="file" ref={this.chooser} onChange={this.handleOnChange} />
-        <div className="urls">
-          {this.state.urls.length > 0 && (
-            <List
-              bordered
-              dataSource={this.state.urls}
-              renderItem={item => <List.Item onClick={() => copy(item)}>{item}</List.Item>}
-            />
-          )}
-          {this.state.percent > 0 && (
-            <Progress
-              percent={this.state.percent}
-              strokeColor={{
-                from: '#108ee9',
-                to: '#87d068'
-              }}
-              percent={99.9}
-              status="active"
-            />
-          )}
+      <>
+        <PageHeader title="闪龙云存储" subTitle="简洁，快速的上传体验" />
+        <div className="page page-upload">
+          <br />
+          <Input
+            addonAfter={
+              <span onClick={this.trigger}>
+                <Icon type="folder-open" />
+              </span>
+            }
+            placeholder="暂未选择任何文件"
+            value={this.state.fileNames}
+            disabled
+          />
+          <input id="file_chooser" multiple type="file" ref={this.chooser} onChange={this.handleOnChange} />
+          <div className="urls">
+            {this.state.urls.length > 0 && (
+              <List
+                bordered
+                dataSource={this.state.urls}
+                renderItem={item => <List.Item onClick={() => copy(item)}>{item}</List.Item>}
+              />
+            )}
+            {this.state.percent > 0 && (
+              <Progress
+                percent={this.state.percent}
+                strokeColor={{
+                  from: '#108ee9',
+                  to: '#87d068'
+                }}
+                percent={99.9}
+                status="active"
+              />
+            )}
+          </div>
         </div>
-      </div>
+        <Footer />
+      </>
     );
   }
 }
